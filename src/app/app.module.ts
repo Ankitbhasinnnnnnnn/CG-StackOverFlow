492456b697d95a253572bc085ebb30ad07631205
Vanshika <89936851+Vanshika0421@users.noreply.github.com>
2023-04-20 17:22:06 +0530

    login and verify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
+import { VerifyComponent } from './verify/verify.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    VerifyComponent
   ],
   imports: [
     BrowserModule,
