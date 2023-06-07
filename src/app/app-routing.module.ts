c998ee046d11918088a31461863db0ddbde26c00
Shivam Rai <shivam.rai@cginfinity.com>
2023-06-07 12:57:36 +0530

    add-routig for landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { VerificationComponent} from './verification/verification.component';
-import { NewPasswordComponent} from './new-password/new-password.component';
+import { VerificationComponent } from './verification/verification.component';
+import { NewPasswordComponent } from './new-password/new-password.component';
 import { PasswordChangedComponent } from './password-changed/password-changed.component';
 const routes: Routes = [
   { path: '', component: QuestionCommentsComponent },
@@ -17,13 +17,13 @@ const routes: Routes = [
   { path: 'questions', component: QuestionsComponent },
   { path: 'ask-a-question', component: AskAQuestionComponent },
   { path: 'question-comments', component: QuestionCommentsComponent },
-  {path: 'Login', component: LoginComponent },
-  {path: 'forgot-password', component: ForgotPasswordComponent },
-  {path: 'sign-up', component: SignUpComponent },
-  {path: 'verification', component:VerificationComponent },
-  {path: 'new-password', component:NewPasswordComponent},
-  {path:  'password-changed', component:PasswordChangedComponent}
-
+  { path: 'Login', component: LoginComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'verification', component: VerificationComponent },
+  { path: 'new-password', component: NewPasswordComponent },
+  { path: 'password-changed', component: PasswordChangedComponent },
+  { path: 'landing-page', component: LandingPageComponent },
 ];
 
 @NgModule({
