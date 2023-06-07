11aa64534e982145bc666b954f8cbc2c111c5096
Shivam Rai <shivam.rai@cginfinity.com>
2023-06-07 15:47:58 +0530

    final fixes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  constructor(private router: Router) {}
+  isLanding: boolean = false;
+  isLandingPage(): boolean {
+    // Replace 'landing' with the route path of your landing page
+    console.log(this.router.url);
+    if (this.router.url.toString() == '/') {
+      this.isLanding = true;
+    } else {
+      this.isLanding = false;
+    }
+    return this.isLanding;
+  }
 }
